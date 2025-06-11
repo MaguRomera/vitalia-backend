ba25605200c5375b2b256925354207c6037ed87c
María Romera <maguromera@gmail.com>
2025-06-11 20:42:08 -0300

    Backend funciona

--- src/express/app.js
+++ src/express/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getById, getAll } = require('../express/routes/especialidades');
 
 const routes = {
-    especialidades: require('/routes/especialidades'),
-    doctores: require('/routes/doctores'),
-    horarios: require('/routes/horarios'),
-    pacientes: require('/routes/pacientes'),
-    turnos: require('/routes/turnos'),
+    especialidades: require('../express/routes/especialidades'),
+    doctores: require('../express/routes/doctores'),
+    horarios: require('../express/routes/horarios'),
+    pacientes: require('../express/routes/pacientes'),
+    turnos: require('../express/routes/turnos'),
 }
 
 const app = express();
@@ -15,19 +16,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //endpoints especialidad
-app.get(`/especialidades`)
-app.get(`/especialidades/:id`)
+app.get(`/especialidades`, routes.especialidades.getAll)
+app.get(`/especialidades/:id`, routes.especialidades.getById)
 //endpoints doctor
-app.get(`/especialidades/:id/doctores`)
-app.get(`/especialidades/:id/doctores/:id`)
+app.get(`/especialidades/:id/doctores`, routes.doctores.getAll)
+app.get(`/especialidades/:id/doctores/:id`, routes.doctores.getById)
 //endpoints horario
-app.get(`/especialidades/:id/doctores/:id/horarios`)
-app.get(`/especialidades/:id/doctores/:id/horarios/:id`)
+app.get(`/especialidades/:id/doctores/:id/horarios`, routes.horarios.getAll)
+app.get(`/especialidades/:id/doctores/:id/horarios/:id`, routes.horarios.getById)
 //endpoints turno
-app.get(`/turnos`)
-app.get(`/turnos/:id`)
-app.post(`/turnos`)
+app.get(`/turnos`, routes.turnos.getAll )
+app.get(`/turnos/:id`, routes.turnos.getById)
+app.post(`/turnos`, routes.turnos.create)
 //endpoints paciente
-app.get(`/turnos/:id/pacientes`)
-app.get(`/turnos/:id/pacientes/:id`)
-app.post(`/turnos/:id/pacientes`)
+app.get(`/turnos/:id/pacientes`, routes.pacientes.getAll)
+app.get(`/turnos/:id/pacientes/:id`, routes.pacientes.getById)
+app.post(`/turnos/:id/pacientes`, routes.pacientes.create)
+
+module.exports = app;
