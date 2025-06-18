3afbff0dc820eaa5dc4b35fe00f3b607ee9ea1d9
levrino <mlevrino@facultad.sanfrancisco.utn.edu.ar>
2025-06-18 19:35:07 -0300

    arreglo de endpoints

--- src/express/app.js
+++ src/express/app.js
@@ -23,18 +23,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get(`/especialidades`, routes.especialidades.getAll)
 app.get(`/especialidades/:id`, routes.especialidades.getById)
 //endpoints doctor
-app.get(`/especialidades/:id/doctores`, routes.doctores.getAll)
-app.get(`/especialidades/:id/doctores/:id`, routes.doctores.getById)
+app.get(`/doctores`, routes.doctores.getAll)
+app.get(`/doctores/:id`, routes.doctores.getById)
 //endpoints horario
-app.get(`/especialidades/:id/doctores/:id/horarios`, routes.horarios.getAll)
-app.get(`/especialidades/:id/doctores/:id/horarios/:id`, routes.horarios.getById)
+app.get(`/horarios`, routes.horarios.getAll)
+app.get(`/horarios/:id`, routes.horarios.getById)
 //endpoints turno
 app.get(`/turnos`, routes.turnos.getAll )
 app.get(`/turnos/:id`, routes.turnos.getById)
 app.post(`/turnos`, routes.turnos.create)
 //endpoints paciente
-app.get(`/turnos/:id/pacientes`, routes.pacientes.getAll)
-app.get(`/turnos/:id/pacientes/:id`, routes.pacientes.getById)
-app.post(`/turnos/:id/pacientes`, routes.pacientes.validateCreatePaciente, routes.pacientes.create)
+app.get(`/pacientes`, routes.pacientes.getAll)
+app.get(`/pacientes/:id`, routes.pacientes.getById)
+app.post(`/pacientes`, routes.pacientes.validateCreatePaciente, routes.pacientes.create)
 
 module.exports = app;
